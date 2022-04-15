446981b25c196e44a0f0991c6646da3fa7c2222c
DivyankaThakur03 <85764700+DivyankaThakur03@users.noreply.github.com>
2022-04-15 23:40:40 +0530

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+#install Python Imaging Library
+pip3 install Pillow
+#install OpenCV
+pip3 install opencv-python
+#install numPY
+pip3 install numpy
+#install pytesseract
+pip3 install pytesseract
